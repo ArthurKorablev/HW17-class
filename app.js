df97da413b5f618f32794fe0621f2f719198bb7e
ArthurKorablev <arttr86@gmail.com>
2023-01-11 17:38:05 +0200

    puting validation

--- app.js
+++ app.js
@@ -1,26 +1,31 @@
 class Person {
     constructor(name, gender) {
-        this.name = name;
-        this.gender = gender;
+        this.name = name.trim();
+        this.gender = gender.trim();
     }
 }
 
 class Flat {
     peopleLiveInFlat = [];
 
-    addPerson(person) {
-        this.peopleLiveInFlat.push(person);
+    set addPerson(person) {
+        if (person instanceof Person) {
+           this.peopleLiveInFlat.push(person); 
+        }   
     }
 }
 
 class House {
     constructor(maxOfFlats) {
-        this.maxOfFlats = maxOfFlats;
+        if (!Number.isNaN(maxOfFlats)) {
+            this.maxOfFlats = maxOfFlats;
+        }
+
         this.flats = [];
     }
 
-    addFlat(flat) {
-        if (this.flats.length < this.maxOfFlats) {
+    set addFlat(flat) {
+        if (this.flats.length < this.maxOfFlats && flat instanceof Flat) {
             this.flats.push(flat);
         } else {
             console.log('This house is full');
@@ -35,10 +40,10 @@ console.log(person1, person2);
 const flat1 = new Flat();
 const flat2 = new Flat();
 
-flat1.addPerson(person1);
-flat1.addPerson(person2);
+flat1.addPerson = person1;
+flat1.addPerson = person2;
 console.log(flat1);
 
 const house1 = new House(1);
-house1.addFlat(flat1);
+house1.addFlat = flat1;
 console.log(house1);
